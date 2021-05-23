a36c8779c3ff65e206f0a3403efde9ff55723895
naoto-honda <82433229+naoto-honda@users.noreply.github.com>
2021-05-23 18:36:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('.burger-btn').on('click',function(){
+    $('.burger-btn').toggleClass('close');
+    $('.nav-wrapper').fadeToggle(500);
+    $('body').toggleClass('noscroll');
+  });
+});
